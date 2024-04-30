444eddabedbab791331d5a4cea0de2fc8660a174
Zargham Khan <zargham.khan@leanix.net>
2024-04-30 10:25:23 +0200

    disable flash and add first keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+-- Shorten function name
+
+local keymap = vim.api.nvim_set_keymap
+
+-- keep the yank when pasting over selection
+keymap("v", "p", '"_dP', opts)
