04ca9472a315d7e7f45835d464d2e6717f5a6759
Zargham Khan <zargham.khan@leanix.net>
2024-05-02 00:12:39 +0200

    add seprate config for vscode temporarily

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
+-- do not use lazyVim if editor is vscode
+if vim.fn.exists("g:vscode") ~= 0 then
+  require("vscode.keymaps")
+  require("vscode.options")
+  vim.cmd("source $HOME/.config/nvim/lua/vscode/settings.vim")
+  return
+end
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
---
+++ lua/vscode/keymaps.lua
@@ -0,0 +1,65 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Insert --
+-- Press jk fast to enter
+-- keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<S-k>", ":m .-2<CR>==", opts)
+keymap("v", "<S-j>", ":m .+1<CR>==", opts)
+
+-- keep the yank when pasting over selection
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- yank inside string
+keymap("n", "<leader>`", "+yi`", opts)
+keymap("n", '<leader>"', '"+yi"', opts)
+keymap("n", "<leader>'", "\"+yi'", opts)
+
+-- COPY/Paste from clipboard
+keymap("n", "<leader>p", '"+p', opts)
+keymap("n", "<leader>y", '"+y', opts)
+
+if vim.fn.exists("g:vscode") ~= 0 then
+  return
+end
+
+-- Navigate buffers
+keymap("n", "<Tab>", ":bnext<CR>", opts)
+
+-- source neovim files
+keymap("n", "<leader>sv", ":luafile $MYVIMRC<CR>", opts)
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
