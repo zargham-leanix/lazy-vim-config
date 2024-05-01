23cca7b2dbbbc51399f6bcbe3a375095c45204d4
Zargham Khan <zargham.khan@leanix.net>
2024-05-02 00:04:23 +0200

    Add source keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,6 @@ local keymap = vim.api.nvim_set_keymap
 
 -- keep the yank when pasting over selection
 keymap("v", "p", '"_dP', opts)
+
+-- source neovim after changing config
+keymap("n", "<leader>sv", ":luafile $MYVIMRC<CR>", opts)
