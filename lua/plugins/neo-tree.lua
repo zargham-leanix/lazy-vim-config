d5d0188ee0fa5a220c00035150e1ab1c8e8bc790
Zargham Khan <zargham.khan@leanix.net>
2024-05-02 20:00:57 +0200

    unhide dotfile

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,4 +10,20 @@ return {
       desc = "Explorer NeoTree (Root Dir)",
     },
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show = {},
+      },
+    },
+  },
 }
