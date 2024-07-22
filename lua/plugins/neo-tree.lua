babd1f64845dadcdc1dca1f7169831f74c6d400e
Zargham Khan <zargham.khan@leanix.net>
2024-07-22 21:51:41 +0200

    remove neo tree override

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,13 +2,13 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   keys = {
     -- only to set toggle = false
-    {
-      "<leader>e",
-      function()
-        require("neo-tree.command").execute({ toggle = false, dir = vim.uv.cwd() })
-      end,
-      desc = "Explorer NeoTree (Root Dir)",
-    },
+    -- {
+    --   "<leader>e",
+    --   function()
+    --     require("neo-tree.command").execute({ toggle = false, dir = vim.uv.cwd() })
+    --   end,
+    --   desc = "Explorer NeoTree (Root Dir)",
+    -- },
   },
   opts = {
     window = {
