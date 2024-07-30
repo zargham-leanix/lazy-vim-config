eb4f0e9053f97c6ee0deae11bacfa59d6f5e684c
Zargham Khan <zargham.khan@leanix.net>
2024-07-30 10:14:42 +0200

    set language and open float

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,6 @@ vim.opt.hidden = true -- don't know what it does
 vim.opt.sidescrolloff = 8
 vim.g.blamer_enabled = true
 vim.opt.relativenumber = false
+
+vim.cmd("language en_US")
+vim.diagnostic.open_float()
